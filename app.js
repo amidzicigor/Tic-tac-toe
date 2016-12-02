51bfbeaafe5e95524d4adbe0980c0a2a8f275f12
Igor Amidzic <amidzicigor@yahoo.com>
2016-12-02 12:08:01 -0500

    Setting up deployment to heroku

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 app.set('view-engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
-hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res) => {
   res.render('home.hbs');
 });
 
-app.listen(3000, () => {
-  console.log('Listening to port 3000');
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
 })
